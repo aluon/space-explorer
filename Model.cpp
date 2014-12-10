2736c8e77c06f09775e6f7edf40b57e6c94b1dd9
Alan Luong <aluong91@gmail.com>
2014-12-10 01:35:43 -0800

    loop through indices

--- Model.cpp
+++ Model.cpp
@@ -19,16 +19,15 @@ void Model::render(Matrix4 matrix)
 {
 	matrix *= transform;
 	glLoadMatrixd(matrix.glMatrix());
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glVertexPointer(3, GL_FLOAT, 0, vertices.data());
-	if (!normals.empty()) {
-		glEnableClientState(GL_NORMAL_ARRAY);
-		glNormalPointer(GL_FLOAT, 0, normals.data());
+	glBegin(GL_TRIANGLES);
+	{
+		for (size_t i = 0; i < vertIndices.size(); ++i) {
+			size_t vertIndex = vertIndices[i] * 3;
+			size_t normIndex = normIndices[i] * 3;
+			size_t texIndex= texIndices[i] * 3;
+			glVertex3f(vertices[vertIndex + 0], vertices[vertIndex + 1], vertices[vertIndex + 2]);
+			glNormal3f(normals[normIndex + 0], normals[normIndex + 1], normals[normIndex + 2]);
+		}
 	}
-	glEnableClientState(GL_COLOR_ARRAY);
-	glColorPointer(3, GL_FLOAT, 0, colors.data());
-	//glDrawElements(GL_TRIANGLES, faces.size(), GL_UNSIGNED_INT, faces.data());
-	glDisableClientState(GL_COLOR_ARRAY);
-	glDisableClientState(GL_NORMAL_ARRAY);
-	glDisableClientState(GL_VERTEX_ARRAY);
+	glEnd();
 }
